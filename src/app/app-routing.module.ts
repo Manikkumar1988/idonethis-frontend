881c105ab11bed0125573886a6a732ca28b9d411
Vineeth <kasimurugasen1995@gmail.com>
2019-11-07 22:11:33 +0530

    routing path added for register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { from } from 'rxjs';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: '**', component: AppComponent}
 ];
 
